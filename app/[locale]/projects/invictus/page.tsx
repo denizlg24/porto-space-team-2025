a5895f7edfad13227571aa7578f9b2b791f06d83
Deniz Gunes <denizlg24@gmail.com>
2025-06-05 14:32:28 +0100

    Added the under construction section

--- app/[locale]/projects/invictus/page.tsx
+++ app/[locale]/projects/invictus/page.tsx
@@ -3,8 +3,10 @@ import bg from "@/public/PLUTUS.png";
 import logoSrc from "@/public/project/INVICTUS.svg";
 import invictusRocket from "@/public/project/invictus-rocket.webp";
 import invictusRocketHorizontal from "@/public/project/invictus-rocket-horizontal.png";
+import invictusCover from "@/public/project/invictus-under-cover.webp";
 import { useTranslations } from "next-intl";
 import { Card, CardHeader } from "@/components/ui/card";
+import { FaTools } from "react-icons/fa";
 
 export default function Page() {
   const t = useTranslations("common");
@@ -97,7 +99,45 @@ export default function Page() {
           </Card>
         </div>
       </div>
-
+      <div className="flex flex-col items-stretch w-full max-w-3xl sm:gap-4 gap-0">
+        <div className="flex flex-col gap-4 w-full col-span-3 ">
+          <div className="flex flex-row items-center justify-center gap-6 w-full">
+            <h2 className="md:text-6xl sm:text-5xl text-3xl font-bebas text-white sm:text-left text-center w-full">
+              {t("project")}{" "}
+              <span className="md:text-6xl sm:text-5xl text-3xl">
+                {t("invictus")}
+              </span>{" "}
+              <span>2025</span>
+            </h2>
+            <Image
+              src={logoSrc}
+              width={1080}
+              height={1080}
+              alt="Invictus logo"
+              className={
+                "w-full h-auto aspect-square md:max-w-48 sm:max-w-32 max-w-24 -rotate-48"
+              }
+            />
+          </div>
+          <div className="w-full grid grid-cols-2 items-center justify-center gap-2">
+            <Image
+              src={invictusCover}
+              alt="cover photo"
+              width={1080}
+              height={1080}
+              className="w-full max-w-3xs mx-auto h-auto object-cover"
+            />
+            <div className="bg-yellow-400 aspect-square w-fit sm:p-6 p-4 rounded-xl flex items-center justify-center rotate-45 relative">
+              <div className="absolute border-black border-4 bg-yellow-400 w-8/9 h-8/9 rounded-xl"></div>
+              <p className="font-bebas sm:text-3xl text-xl text-black text-center -rotate-45">
+                {t("under")} <br />
+                {t("construction")}
+                <FaTools className="text-black z-10 mx-auto mt-2" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
       <Image
         src={bg}
         alt="cover photo"
